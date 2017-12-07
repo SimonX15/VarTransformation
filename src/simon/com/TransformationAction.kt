ceed15129e84813f9590e0ab6bd001202577cdc0
xiaowei <SimonX15@163.com>
2017-12-07 15:16:43 +0800

    下划线及后面的小写字母，转大写

--- src/simon/com/TransformationAction.kt
+++ src/simon/com/TransformationAction.kt
@@ -12,17 +12,59 @@ import com.intellij.psi.util.PsiUtilBase
 class TransformationAction : AnAction() {
 
     override fun actionPerformed(anActionEvent: AnActionEvent) {
-        //当前的项目和文件信息
         val project = anActionEvent.getData(PlatformDataKeys.PROJECT)
         val editor = anActionEvent.getData(PlatformDataKeys.EDITOR)
-        /*PsiFile currentEditorFile = PsiUtilBase.getPsiFileInEditor(editor, project);
-        String currentEditorFileName = currentEditorFile.getName();*/
+        //        editor.getSelectionModel().getSelectedText()
 
-        //当前选中的内容
-        val selectedText = editor!!.selectionModel.selectedText
+        //对选中的变量进行处理
+        if (editor != null && project != null) {
+            //选择的内容
+            val selectedText = editor.selectionModel.selectedText
+            if (selectedText.isNullOrEmpty()) {
+                return
+            }
 
-        /*
+            //通过下划线分割，并且下划线不是第一个字符
+            if (selectedText!!.contains("_")) {
+                val splitWords = selectedText.split("_")
+                //驼峰
+                var humpWords = ""
+                for (index in 0 until splitWords.size) {
+                    val word = splitWords[index]
+                    humpWords += if (index == 0) {
+                        word
+                    } else {
+                        when (word.length) {
+                        //只有一个单词，直接转大写
+                            1 -> {
+                                word.toUpperCase()
+                            }
+                        //超过一个，只转第一个为大写
+                            else -> {
+                                val first2Upper = word[0].toUpperCase()
+                                first2Upper + word.substring(1)
+                            }
+                        }
+                    }
+                }
+
+                //大写
+                val uppercaseWords = selectedText.toUpperCase()
+
+                //小写
+                val lowercaseWords = selectedText.toLowerCase()
+            }
+
+        }
+
+
+        //当前的项目和文件信息
+        //        Project project = anActionEvent.getData(PlatformDataKeys.PROJECT);
+        //        Editor editor = anActionEvent.getData(PlatformDataKeys.EDITOR);
+        //        PsiFile currentEditorFile = PsiUtilBase.getPsiFileInEditor(editor, project);
+        //        String currentEditorFileName = currentEditorFile.getName();
+
         //显示一个对话框
-        Messages.showMessageDialog("Hello World !", "Information", Messages.getInformationIcon());*/
+        //        Messages.showMessageDialog("Hello World !", "Information", Messages.getInformationIcon());
     }
 }
